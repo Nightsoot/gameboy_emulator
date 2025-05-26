561127d8d452c58728e2326049e744d9e0d879f2
nightsoot <nightsoot@gmail.com>
2025-05-26 16:05:53 -0500

    Block 0 and Block 1 finished (except HALT)

--- scripts/opcodes.py
+++ scripts/opcodes.py
@@ -7,7 +7,7 @@ u8_regs_read = [
     "registers.DE_reg.sub.E",
     "registers.HL_reg.sub.H",
     "registers.HL_reg.sub.L",
-    "write_memory(registers.HL_reg.HL)",
+    "read_memory(registers.HL_reg.HL)",
     "registers.AF_reg.sub.A",
 ]
 
@@ -18,7 +18,7 @@ u8_regs_write = [
     "registers.DE_reg.sub.E",
     "registers.HL_reg.sub.H",
     "registers.HL_reg.sub.L",
-    "read_memory(registers.HL_reg.HL)",
+    "write_memory(registers.HL_reg.HL)",
     "registers.AF_reg.sub.A",
 ]
 
@@ -56,6 +56,13 @@ cond = [
     "!(registers.AF_reg.sub.F_reg.flags.c)",
     "(registers.AF_reg.sub.F_reg.flags.c)",
 ]
+
+flags = {
+    "c": "registers.AF_reg.sub.F_reg.flags.c",
+    "h": "registers.AF_reg.sub.F_reg.flags.h",
+    "z": "registers.AF_reg.sub.F_reg.flags.z",
+    "n": "registers.AF_reg.sub.F_reg.flags.n",
+}
 
 
 def ld_ops():
@@ -100,4 +107,145 @@ def dec_r16():
         print(f"    {u16_regs[i]}--;")
         print(f"    return 8;")
         
-ld_ops();
+def add_r16():
+    const = 0b00001001
+    for i in range(0, 4):
+        print(f"case(0x{(const + (i<<4)):02X}):")
+        print(f"    {flags['n']} = 0;")
+        print(f"    {flags['c']} = tell_overflow(registers.HL_reg.HL, {u16_regs[i]}, 0, 15);")
+        print(f"    {flags['h']} = tell_overflow(registers.HL_reg.HL, {u16_regs[i]}, 0, 11);")
+        print(f"    registers.HL_reg.HL = registers.HL_reg.HL + {u16_regs[i]};")
+        print(f"    return 8;")
+        
+def inc_r8():
+    const = 0b00000100
+    for i in range(0, 8):
+        print(f"case(0x{(const + (i<<3)):02X}):")
+        print(f"    {flags['n']} = 0;")
+        print(f"    {flags['h']} = tell_overflow({u8_regs_read[i]}, {u8_regs_read[i]}, 0, 3);")
+        print(f"    {u8_regs_write[i]}++;")
+        print(f"    {flags['z']} = ({u8_regs_read[i]} == 0);")
+        print(f"    return 4;")
+        
+def dec_r8():
+    const = 0b00000101
+    for i in range(0, 8):
+        print(f"case(0x{(const + (i<<3)):02X}):")
+        print(f"    {flags['n']} = 1;")
+        print(f"    {flags['h']} = tell_borrow({u8_regs_read[i]}, {u8_regs_read[i]}, 4);")
+        print(f"    {u8_regs_write[i]}--;")
+        print(f"    {flags['z']} = ({u8_regs_read[i]} == 0);")
+        print(f"    return 4;")
+    
+def ld_r8():
+    const = 0b00000110
+    for i in range(0, 8):
+        print(f"case(0x{(const + (i<<3)):02X}):")
+        print(f"    {u8_regs_write[i]} = imm8;")
+        print(f"    return 8;")
+        
+def misc_block0():
+    #RLCA
+    print(f"case(0x07):")
+    print(f"    {flags['n']} = 0;")
+    print(f"    {flags['h']} = 0;")
+    print(f"    {flags['z']} = 0;")
+    print(f"    {flags['c']} = BIT({u8_regs_write[7]}, 7);")
+    print(f"    {u8_regs_write[7]} = ({u8_regs_write[7]} << 1) | BIT({u8_regs_write[7]}, 7);")
+    print(f"    return 4;")
+    
+    #RRCA
+    print(f"case(0x0F):")
+    print(f"    {flags['n']} = 0;")
+    print(f"    {flags['h']} = 0;")
+    print(f"    {flags['z']} = 0;")
+    print(f"    {flags['c']} = BIT({u8_regs_write[7]}, 0);")
+    print(f"    {u8_regs_write[7]} = ({u8_regs_write[7]} >> 1) | (BIT({u8_regs_write[7]}, 0) << 7);")
+    print(f"    return 4;")
+    
+    #RLA
+    print(f"case(0x17):")
+    print(f"    uint8_t prev_carry = {flags['c']};")
+    print(f"    {flags['n']} = 0;")
+    print(f"    {flags['h']} = 0;")
+    print(f"    {flags['z']} = 0;")
+    print(f"    {flags['c']} = BIT({u8_regs_write[7]}, 7);")
+    print(f"    {u8_regs_write[7]} = ({u8_regs_write[7]} << 1) | prev_carry;")
+    print(f"    return 4;")
+    
+    #RRA
+    print(f"case(0x1F):")
+    print(f"    uint8_t prev_carry = {flags['c']};")
+    print(f"    {flags['n']} = 0;")
+    print(f"    {flags['h']} = 0;")
+    print(f"    {flags['z']} = 0;")
+    print(f"    {flags['c']} = BIT({u8_regs_write[7]}, 0);")
+    print(f"    {u8_regs_write[7]} = ({u8_regs_write[7]} >> 1) | (prev_carry << 7);")
+    print(f"    return 4;")
+    
+    #DAA
+    print(f"case(0x27):")
+    print(f"    {flags['h']} = 0;")
+    print(f"    uint8_t adj = 0;")
+    print(f"    if({flags['n']})")
+    print(f"        adj += ({flags['h']}) ? 0x6 : 0;")
+    print(f"        adj += ({flags['c']}) ? 0x60: 0;")
+    print(f"        {u8_regs_write[7]} -= adj;")
+    print(f"    else")
+    print(f"        adj += ({flags['h']} || (({u8_regs_write[7]} & 0xF) > 0x9)) ? 0x6: 0;")
+    print(f"        if({flags['c']} || {u8_regs_read[7]} > 0x99)")
+    print(f"            adj += 0x60;")
+    print(f"            {flags['c']} = 1;")
+    print(f"        {u8_regs_write[7]} += adj;")
+    print(f"    {flags['z']} = ({u8_regs_read[7]} == 0);")
+    print(f"    return 4;")
+    
+    #CPL
+    print(f"case(0x2F):")
+    print(f"    {flags['h']} = 1;")
+    print(f"    {flags['n']} = 1;")
+    print(f"    {u8_regs_write[7]} = ~{u8_regs_write[7]};")
+    print(f"    return 4;")
+    
+    #SCF
+    print(f"case(0x37):")
+    print(f"    {flags['h']} = 0;")
+    print(f"    {flags['n']} = 0;")
+    print(f"    {flags['c']} = 1;")
+    print(f"    return 4;")
+    
+    #CCF
+    print(f"case(0x3F):")
+    print(f"    {flags['h']} = 0;")
+    print(f"    {flags['n']} = 0;")
+    print(f"    {flags['c']} = ~{flags['c']};")
+    print(f"    return 4;")
+  
+def JR():
+    #JR
+    print(f"case(0x18):")
+    print(f"    registers.PC += ((int8_t) imm8);")  
+    print(f"    return 12;")
+    
+    #JRC
+    const = 0b00100000
+    for i in range(0, 4):
+        print(f"case(0x{(const + (i<<3)):02X}):")
+        print(f"    if({cond[i]})")
+        print(f"    registers.PC += ((int8_t) imm8);")
+        print(f"    return 12;")
+        print(f"    return 8;")
+    
+    print(f"case(0x01):")
+    print(f'    printf("STOP HIT\ n");')
+    print(f"    return 0;")
+
+def ld_r8_r8():
+    const = 0b01000000
+    for i in range(0, 64):
+        print(f"case(0x{(const + (i<<0)):02X}):")
+        print(f"    {u8_regs_write[(i & (0b111 << 3)) >> 3]} = {u8_regs_read[i & 0b111]};")
+        print(f"    return 4;")
+
+  
+ld_r8_r8();
--- src/opcodes.c
+++ src/opcodes.c
@@ -1,11 +1,20 @@
 #include "../includes/opcodes.h"
 
+//tell overflow at a given bit
 uint8_t tell_overflow(uint16_t a, uint16_t b, uint8_t cin, uint8_t bit){
     uint32_t carry = cin;
-    for(int i=1; i<bit; i++){
-        carry |= ((BIT(a,i-1) + BIT(b,i-1) + BIT(carry, i-1)) & 0b1) << i;
+    for(int i=1; i<=bit+1; i++){
+        carry |= ((BIT(a, i - 1) + BIT(b, i - 1) + BIT(carry, i - 1)) & 0b10) << (i-1);
     }
-    return (BIT(carry, bit) ^ BIT(carry, bit-1));
+    return (BIT(carry, bit) ^ BIT(carry, bit+1));
+}
+
+uint8_t tell_borrow(uint16_t a, uint16_t b, uint8_t bit){
+    uint16_t borrow = BIT(b, 15) > BIT(a, 15);
+    for(int i=14; i>=bit; i--){
+        borrow |= (BIT(b, i) > (BIT(a, i), BIT(borrow, i+1))) << i;
+    }
+    return BIT(borrow, bit);
 }
 
 uint8_t no_prefix_opcodes(uint8_t opcode, uint16_t imm16, uint8_t imm8){
@@ -50,6 +59,90 @@ uint8_t no_prefix_opcodes(uint8_t opcode, uint16_t imm16, uint8_t imm8){
     case (0x08):
         write_memory(imm16, registers.SP);
         return 20;
+    case (0x09):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.c = tell_overflow(registers.HL_reg.HL, registers.BC_reg.BC, 0, 15);
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.HL_reg.HL, registers.BC_reg.BC, 0, 11);
+        registers.HL_reg.HL = registers.HL_reg.HL + registers.BC_reg.BC;
+        return 8;
+    case (0x19):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.c = tell_overflow(registers.HL_reg.HL, registers.DE_reg.DE, 0, 15);
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.HL_reg.HL, registers.DE_reg.DE, 0, 11);
+        registers.HL_reg.HL = registers.HL_reg.HL + registers.DE_reg.DE;
+        return 8;
+    case (0x29):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.c = tell_overflow(registers.HL_reg.HL, registers.HL_reg.HL, 0, 15);
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.HL_reg.HL, registers.HL_reg.HL, 0, 11);
+        registers.HL_reg.HL = registers.HL_reg.HL + registers.HL_reg.HL;
+        return 8;
+    case (0x39):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.c = tell_overflow(registers.HL_reg.HL, registers.SP, 0, 15);
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.HL_reg.HL, registers.SP, 0, 11);
+        registers.HL_reg.HL = registers.HL_reg.HL + registers.SP;
+        return 8;
+    case (0x03):
+        registers.BC_reg.BC++;
+        return 8;
+    case (0x13):
+        registers.DE_reg.DE++;
+        return 8;
+    case (0x23):
+        registers.HL_reg.HL++;
+        return 8;
+    case (0x33):
+        registers.SP++;
+        return 8;
+    case (0x04):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.BC_reg.sub.B, registers.BC_reg.sub.B, 0, 3);
+        registers.BC_reg.sub.B++;
+        registers.AF_reg.sub.F_reg.flags.z = (registers.BC_reg.sub.B == 0);
+        return 4;
+    case (0x0C):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.BC_reg.sub.C, registers.BC_reg.sub.C, 0, 3);
+        registers.BC_reg.sub.C++;
+        registers.AF_reg.sub.F_reg.flags.z = (registers.BC_reg.sub.C == 0);
+        return 4;
+    case (0x14):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.DE_reg.sub.D, registers.DE_reg.sub.D, 0, 3);
+        registers.DE_reg.sub.D++;
+        registers.AF_reg.sub.F_reg.flags.z = (registers.DE_reg.sub.D == 0);
+        return 4;
+    case (0x1C):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.DE_reg.sub.E, registers.DE_reg.sub.E, 0, 3);
+        registers.DE_reg.sub.E++;
+        registers.AF_reg.sub.F_reg.flags.z = (registers.DE_reg.sub.E == 0);
+        return 4;
+    case (0x24):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.HL_reg.sub.H, registers.HL_reg.sub.H, 0, 3);
+        registers.HL_reg.sub.H++;
+        registers.AF_reg.sub.F_reg.flags.z = (registers.HL_reg.sub.H == 0);
+        return 4;
+    case (0x2C):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.HL_reg.sub.L, registers.HL_reg.sub.L, 0, 3);
+        registers.HL_reg.sub.L++;
+        registers.AF_reg.sub.F_reg.flags.z = (registers.HL_reg.sub.L == 0);
+        return 4;
+    case (0x34):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(read_memory(registers.HL_reg.HL), read_memory(registers.HL_reg.HL), 0, 3);
+        write_memory(registers.HL_reg.HL, read_memory(registers.HL_reg.HL) + 1);
+        registers.AF_reg.sub.F_reg.flags.z = (read_memory(registers.HL_reg.HL) == 0);
+        return 12;
+    case (0x3C):
+        registers.AF_reg.sub.F_reg.flags.n = 0;
+        registers.AF_reg.sub.F_reg.flags.h = tell_overflow(registers.AF_reg.sub.A, registers.AF_reg.sub.A, 0, 3);
+        registers.AF_reg.sub.A++;
+        registers.AF_reg.sub.F_reg.flags.z = (registers.AF_reg.sub.A == 0);
+        return 4;
     default:
         printf("BAD OPCODE");
         return -1;
@@ -57,8 +150,15 @@ uint8_t no_prefix_opcodes(uint8_t opcode, uint16_t imm16, uint8_t imm8){
 }
 
 int main(void){
-    uint16_t x = 0xFFFF;
-    uint16_t y = 0x0000;
-    uint8_t overflow = tell_overflow(x,y, 0, 15);
-    printf("OVERFLOW (0x%x + 0x%x): %d\n", x, y, overflow);
+    uint16_t x = 0xEFFF;
+    uint16_t y = 0x1000;
+    uint8_t borrow = tell_borrow(x,y, 0);
+    printf("BORROW (0x%x - 0x%x): %d\n", x, y, borrow);
+
+    no_prefix_opcodes(0x31, 500, 0);
+    printf("SP: %d\n", registers.SP);
+    no_prefix_opcodes(0x39, 500, 0);
+    no_prefix_opcodes(0x39, 500, 0);
+    no_prefix_opcodes(0x23, 0, 0);
+    printf("HL: %d\n", registers.HL_reg.HL);
 }
